30af9429403baf9a046c3bf9ded7b5db78403102
Martmists <martmists@gmail.com>
2022-01-12 00:46:45 +0100

    Suppress warnings from nm
    Signed-off-by: Martmists <martmists@gmail.com>

--- fill_templates.sh
+++ fill_templates.sh
@@ -5,5 +5,5 @@ dir=$2
 source=$3
 out=$4
 
-sed "s|SYMBOLS|$($nm $(find $dir -type f) | grep -E "[TW] _Z" | awk '{print $3}' | xargs | sed "s/ /; /g")|g" $source/exported.txt.template > $out/exported.txt
+sed "s|SYMBOLS|$($nm $(find $dir -type f -name "*.obj") | grep -E "[TW] _Z" | awk '{print $3}' | xargs | sed "s/ /; /g")|g" $source/exported.txt.template > $out/exported.txt
 sed "s,ROOT_DIR,$source,g" $source/switch.specs.template > $out/switch.specs
