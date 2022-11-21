e8885ee7500cdf107e7f244e4dbcaf919c0d0535
David van Zyl <davidvanzyl@gmail.com>
2022-11-21 18:42:39 +0200

    Linked SwayGrass Infotype

--- src/patches/radar_fix_ivs.cpp
+++ src/patches/radar_fix_ivs.cpp
@@ -3,7 +3,8 @@
 #include "util.hpp"
 #include "Dpr/Field/SwayGrass.hpp"
 
-extern Dpr_Field_SwayGrass_c * SwayGrass_InfoType;
+// extern Dpr_Field_SwayGrass_c * SwayGrass_InfoType;
+
 extern void * DAT_7104cbc6f7;
 using namespace Dpr::Field;
 uint8_t Dpr_Field_SwayGrass_RensaTalent(MethodInfo *method)
@@ -12,9 +13,10 @@ uint8_t Dpr_Field_SwayGrass_RensaTalent(MethodInfo *method)
     uint chainLength;
     uint32_t count;
     SwayGrass_StaticFields *staticFields;
-
+    
     // data load as per original code
     system_load_typeinfo(DAT_7104cbc6f7);
+
 
     if ((SwayGrass_InfoType->_2).cctor_finished != 0) {
         staticFields = SwayGrass_InfoType->static_fields;
@@ -35,22 +37,22 @@ uint8_t Dpr_Field_SwayGrass_RensaTalent(MethodInfo *method)
     // checks if the SwayGrass_InfoType chain is active
     // returns IVs based on chain length
     if (isSwaying != '\0') {
-        if (chainLength<20){ // 
+        if (chainLength<2){ // 
             return 0;
         }
-        if (chainLength<30){ // 20-30
+        if (chainLength<3){ // 20-30
             return 1;
         }
-        if (chainLength<40){ // 30-40
+        if (chainLength<4){ // 30-40
             return 2;
         }
-        if (chainLength<70){// 40-70
+        if (chainLength<7){// 40-70
             return 3;
         }
-        if (chainLength<100){// 70-100
+        if (chainLength<10){// 70-100
             return 4;
         }
-        if (chainLength>100){
+        if (chainLength>10){
             return 5;
         }
     }
