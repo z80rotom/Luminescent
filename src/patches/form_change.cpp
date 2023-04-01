061447228305edd9d54fce3ea04d3336014021a3
SaltContainer <fanmc34@gmail.com>
2023-03-31 21:59:31 -0400

    Mewtwo armor fix

--- src/patches/form_change.cpp
+++ src/patches/form_change.cpp
@@ -11,14 +11,15 @@ bool DecideFormNoFromHoldItem(int32_t monsno, uint32_t holdItemno, uint16_t *for
     int32_t GIRATINA_MONSNO = 0x1E7;
     int32_t ARCEUS_MONSNO = 0x1ED;
     uint32_t GIRATINA_ORB_ITEMNO = 0x70;
-    uint32_t MEWTWO_ARMOR_ITEMNO = 0x721;
+    uint32_t MEWTWO_ARMOR_ITEMNO = 0x72A;
+    uint16_t MEWTWO_ARMOR_FORMNO = 0x3;
 
     if (monsno == GIRATINA_MONSNO) {
         *formno = holdItemno == GIRATINA_ORB_ITEMNO;
     } else if (monsno == ARCEUS_MONSNO) {
         *formno = Pml::PokePara::CalcTool::GetAruseusuType(holdItemno, (MethodInfo *) nullptr);
     } else if (monsno == MEWTWO_MONSNO) {
-        *formno = holdItemno == MEWTWO_ARMOR_ITEMNO;
+        *formno = holdItemno == MEWTWO_ARMOR_ITEMNO ? MEWTWO_ARMOR_FORMNO : 0;
     } else {
         return false;
     }
@@ -29,7 +30,7 @@ bool DecideFormNoFromHoldItem(int32_t monsno, uint32_t holdItemno, uint16_t *for
 bool CheckUnbreakablePokeItem(uint16_t monsno,uint16_t itemID,MethodInfo *method)
 {
     uint16_t MEWTWO_MONSNO = 0x96;
-    uint16_t MEWTWO_ARMOR_ITEMNO = 0x721;
+    uint16_t MEWTWO_ARMOR_ITEMNO = 0x72A;
     if (monsno == MEWTWO_MONSNO && itemID == MEWTWO_ARMOR_ITEMNO) {
         return true;
     }
