de50d9477d0beb90bf1e18dfa99deba3a926284c
SaltContainer <fanmc34@gmail.com>
2023-04-20 00:59:10 -0400

    Custom Script Commands framework

--- src/patches/custom_script.cpp
+++ src/patches/custom_script.cpp
@@ -1,16 +1,60 @@
 #include "il2cpp.hpp"
+#include "Dpr/EvScript/EvDataManager.hpp"
+#include "EvData/EvCmdID.hpp"
+#include "EvData/ArgType.hpp"
 #include "PlayerWork.hpp"
+#include "WeatherWork.hpp"
 
-namespace Dpr
+#include "logger.hpp"
+
+float ConvertToFloat(int32_t value)
 {
-    namespace EvScript
-    {
-        struct EvDataManager_o;
-    }
+    float fresult;
+    std::memcpy(&fresult, &value, sizeof(value));
+    return fresult;
 }
 
-// bool Dpr_EvScript_EvDataManager_EvCmdTrainerFlagCheck(Dpr::EvScript::EvDataManager_o *__this,MethodInfo *method)
-// {
-//     *((int32_t *) NULL) = 12345;
-//     return true;
-// }
+bool SetWeather(Dpr::EvScript::EvDataManager_o * manager)
+{
+    socket_log_fmt("SetWeather\n");
+    System::Array<EvData::Aregment_o>* args = manager->fields._evArg;
+
+    if (args->max_length >= 2)
+    {
+        int32_t argType = args->m_Items[1].fields.argType;
+        int32_t data = args->m_Items[1].fields.data;
+        int32_t weather = 0;
+
+        switch (argType)
+        {
+            case EvData::ArgType::Work:
+                weather = PlayerWork::GetInt(data, nullptr);
+                break;
+            case EvData::ArgType::Float:
+                weather = (int32_t)ConvertToFloat(data);
+                break;
+            default:
+                break;
+        }
+
+        socket_log_fmt("Calling set_WeatherID with weatherId: %d\n", weather);
+        WeatherWork::set_WeatherID(weather, nullptr);
+    }
+
+    return true;
+}
+
+bool RunEvCmdExtended(Dpr::EvScript::EvDataManager_o *__this, EvData::EvCmdID index, MethodInfo *method)
+{
+    // Overriden/New Commands
+    switch (index)
+    {
+        case EvData::EvCmdID::_SET_WEATHER:
+            return SetWeather(__this);
+        default:
+            break;
+    }
+
+    // Call original method
+    return __this->RunEvCmd(index, method);
+}
